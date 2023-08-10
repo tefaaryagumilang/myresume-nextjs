1e38ff0547470926c01ba6dfb4b4a6f702453f8d
tefaarya27 <tefa.arya@digiform.co.id>
2023-08-10 20:44:20 +0700

    Update pages

--- src/app/education/page.js
+++ src/app/education/page.js
@@ -84,7 +84,7 @@ export default function Education() {
         <Footer />
         
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></Script>
-        <Script src="/js/scripts.js"></Script>
+        <Script src="js/scripts.js"></Script>
         <Script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossOrigin="anonymous"></Script>
         </>
   )
--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -136,7 +136,7 @@ export default function Portfolio() {
         <Footer />
         
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></Script>
-        <Script src="/js/scripts.js"></Script>
+        <Script src="js/scripts.js"></Script>
         <Script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossOrigin="anonymous"></Script>
         </>
   )
